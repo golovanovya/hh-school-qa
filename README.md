fcee305f4e38601f738c9077804f5b810f3991f3
golovanovya <golovanovya@gmail.com>
2020-12-10 14:10:48 +0300

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+# HH QA Homework
+
+## Installation
+
+Clone from github
+
+    git clone git@github.com:golovanovya/hh-school-qa.git
+
+Copy `.env.example` to `.env` file and edit `TOKEN` option
+
+    cp .env.example .env
+
+Install npm packages
+
+    npm install
+
+## Run tests
+
+Run command
+
+    npm run test
---
+++ test/search_test.js
@@ -0,0 +1,60 @@
+require('dotenv').config();
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+chai.use(chaiHttp);
+const expect = chai.expect;
+const should = chai.should();
+
+const token = process.env.TOKEN;
+const baseUrl = 'https://api.hh.ru';
+
+const makeRequest = () => chai.request(baseUrl)
+    .get('/vacancies')
+    .set('Authorization', `Bearer ${token}`)
+    .set('Content-Type', 'application/json');
+
+describe('credentials', () => {
+    it('token exists', () => {
+        should.exist(token, "please add TOKEN to environments");
+        expect(token).to.be.a('string');
+    });
+});
+
+describe(baseUrl, () => {
+    describe('/vacancies', () => {
+        describe('search field', () => {
+            it('wrong request', () => {
+                chai.request(baseUrl)
+                    .get('/vacanciess')
+                    .query({text: 'test'})
+                    .end((err, res) => {
+                        expect(err).to.be.null;
+                        expect(res).to.have.status(404);
+                    })
+            });
+            const assertions = [
+                ['frontend разработчик', [/frontend/gmi, /разраб/gmi]],
+                ['"frontend разработчик"', [/frontend.?разработчик/gmi]],
+                ['!разработчика', [/разработчика/gmi]],
+                ['NAME:(python OR java) and COMPANY_NAME:Headhunter', [/Java developer/gmi]],
+            ];
+            assertions.forEach(([text, expectation]) => {
+                it(text, () => {
+                    makeRequest().query({text: text})
+                        .end((err, res) => {
+                            expect(err).to.be.null;
+                            res.should.to.have.status(200)
+                                .to.be.json;
+                            res.body.items.should.be.a('array')
+                                .to.have.lengthOf.above(0);
+                            const bodyString = JSON.stringify(res.body.items);
+                            expectation.forEach(exp => {
+                                expect(bodyString).to.match(exp);
+                            })
+                        });
+                });
+            });
+        });
+    });
+});
